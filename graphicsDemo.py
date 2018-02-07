072861582f5488a82c25dd2981e64a68cd71196c
matty2cat1 <35492827+matty2cat1@users.noreply.github.com>
2018-02-07 08:18:41 -0500

    Update graphicsDemo.py

--- graphicsDemo.py
+++ graphicsDemo.py
@@ -1,15 +1,21 @@
 #Matt Westelman
 #2/7/18
-#graphicsDemo.py - Intro to game
+#graphicsDemo.py - Intro to ggame
 
 from ggame import *
 
 red = Color(0xFF0000,1) #this is the color red
+green = Color(0x00FF00,1)
+blue = Color(0x0000FF,1)
 black = Color(0x000000,1) #Black
 
 blackOutline = LineStyle(1,black)
 
 redRectangle = RectangleAsset(200,100,blackOutline,red) #Width, height, outline and fill
+blueCircle = CircleAsset(50,blackOutline,blue) #Radius, outline, fill
+greenEllipse = EllipseAsset(100, 50, blackOutline, green) #Width, height, outline, fill
 
 Sprite(redRectangle)
+Sprite(blueCircle,(50,50))
+Sprite(greenEllipse,(200,400))
 App().run()
